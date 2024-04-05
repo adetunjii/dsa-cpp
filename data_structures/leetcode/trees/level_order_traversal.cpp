9dfb14dbff6ed1b1a024aa23220e5b0b6321172c
adetunjii <adetunjithomas1@outlook.com>
2024-04-05 11:30:33 +0100

    tree: level order traversal

---
+++ data_structures/leetcode/trees/level_order_traversal.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+#include "treenode.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+
+        if (root == nullptr) {
+            return res;
+        }
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        res.push_back({root->val});
+
+        while(!q.empty()) {
+            vector<int> temp;
+            for (int i = 0; i < q.size(); i++) {
+                TreeNode* top = q.front();
+                q.pop();
+
+                temp.push_back(top->val);
+
+                if (top->left != nullptr) q.push(top->left);
+                if (top->right != nullptr) q.push(top->right);
+            }
+
+            res.push_back(temp);
+        }
+
+        return res;
+    }
+};
