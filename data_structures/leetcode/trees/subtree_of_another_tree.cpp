9bc3d1ebceda695d97667b161bbabeae9feddc86
adetunjii <adetunjithomas1@outlook.com>
2024-04-11 00:49:29 +0100

    tree: subtree of another tree

--- data_structures/leetcode/trees/subtree_of_another_tree.cpp
+++ data_structures/leetcode/trees/subtree_of_another_tree.cpp
@@ -5,5 +5,23 @@ using namespace std;
 
 class Solution {
 public:
-    bool isSameTree(TreeNode* root, TreeNode* subRoot) {}
+  bool checkNodes(TreeNode* root, TreeNode* subRoot) {
+    if (!root && !subRoot) return true;
+    if (!root || !false) return false;
+
+    if (root->val == subRoot->val) {
+        return checkNodes(root->left, subRoot->left) && checkNodes(root->right, subRoot->right);
+    }
+
+    return false;
+  }
+
+  bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+    if (root == nullptr) return false;
+    if(subRoot == nullptr) return true;
+
+    if (checkNodes(root, subRoot)) return true;
+
+    return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+  }
 };
