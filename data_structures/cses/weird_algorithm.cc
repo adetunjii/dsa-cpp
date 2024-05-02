81249d0a374241d1cae44906601baa61872c00c3
adetunjii <adetunjithomas1@outlook.com>
2024-05-02 22:53:02 +0100

    cses: weird algorithm

---
+++ data_structures/cses/weird_algorithm.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve(long long n) {
+
+	while (n != 1) { 
+		cout << n << " "; 
+		n = n & 1LL ? (n*3) + 1 : n / 2;
+	}
+	cout << n << "\n";
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N;
+	while (cin >> N ) {
+		solve(N);
+	}
+
+	return 0;
+}
