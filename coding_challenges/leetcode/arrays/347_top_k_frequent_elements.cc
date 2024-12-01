5c245bbbe9202aeca6adf6a08f73e490d6c29836
adetunjii <adetunjithomas1@outlook.com>
2024-12-01 01:47:45 +0100

    347. top k frequent elements

---
+++ coding_challenges/leetcode/arrays/347_top_k_frequent_elements.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> topKFrequentElement(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        for (int num : nums) m[num] += 1;
+
+        vector<vector<int>> bucket(nums.size() + 1);
+        for (const auto& [key, val] : m) {
+           bucket[val].push_back(key); 
+        }
+
+        vector<int> res;
+        for(int j = bucket.size() - 1; j >= 0; j--) {
+            for (int n : bucket[j])
+                res.push_back(n);
+                if (res.size() == k) return res;
+        }
+
+        return {};
+    }
+};
