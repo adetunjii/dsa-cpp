cfded155956313e8e64f97261c63188b4ad8b6f2
adetunjii <adetunjithomas1@outlook.com>
2024-12-11 23:34:14 +0100

    238 product of array except self

---
+++ coding_challenges/leetcode/arrays/238_product_of_array_except_self.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution{
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int N = nums.size();
+        vector<int> res(N, 1);
+
+        int x = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            res[i] = x;
+            x *= nums[i-1];
+        }
+
+        int prev = 1;
+        for (int j = N - 1; j >= 0; j--) {
+            res[j] *= prev;
+            prev *= nums[j+1];
+        }
+
+        return res;
+    }
+};
