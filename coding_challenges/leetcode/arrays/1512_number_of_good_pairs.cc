3c1b503c9c52bbc9938c36879e2f056c257cf1e9
adetunjii <adetunjithomas1@outlook.com>
2025-01-09 00:28:10 +0100

    1512. Number of good pairs

--- coding_challenges/leetcode/arrays/1512_number_of_good_pairs.cc
+++ coding_challenges/leetcode/arrays/1512_number_of_good_pairs.cc
@@ -17,4 +17,19 @@ public:
 
         return res;
     }
+
+    int numIdenticalPairs(vector<int>& nums) {
+        int N = nums.size();
+        unordered_map<int, int> m;
+        int res = 0;
+        for (int i = 0; i < N; i++)  {
+            m[nums[i]] += 1;
+        }       
+
+        for (const auto& [key, value] : m) {
+            res += value * (value - 1) / 2;
+        }
+
+        return res;
+    }
 };
