e078f6b576846dfa6a576e6053f8128660d5ac6e
adetunjii <adetunjithomas1@outlook.com>
2025-01-15 00:13:29 +0100

    88. Merge sorted arrays

--- coding_challenges/leetcode/arrays/88_merge_sorted_array.cc
+++ coding_challenges/leetcode/arrays/88_merge_sorted_array.cc
@@ -5,6 +5,16 @@ using namespace std;
 
 class Solution {
 public:
+    void merge_brute(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int l = m + n - 1;
+        while (--n >= 0) {
+            nums1[l] = nums2[n];
+            l -= 1;
+        }
+
+        sort(nums1.begin(), nums1.end());
+    }
+
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         while(n > 0) {
             if (m == 0 || nums2[n-1] >= nums1[m-1]) {
