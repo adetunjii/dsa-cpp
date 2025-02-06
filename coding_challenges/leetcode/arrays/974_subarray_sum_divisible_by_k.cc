6c530e4426697acda93e7ff1829063eb8d7dcc9c
adetunjii <adetunjithomas1@outlook.com>
2025-02-06 23:53:32 +0100

    974. Subarray sum divisible by k

---
+++ coding_challenges/leetcode/arrays/974_subarray_sum_divisible_by_k.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int N = nums.size();
+        int count = 0;
+        unordered_map<int, int> m;
+        int sum = 0;
+        m[0] = 1;
+        for (int i = 0; i < N; i++) {
+            sum += nums[i];
+            int rem = ((sum % k) + k) % k;
+            if (m.find(rem) != m.end())
+                count += m[rem];
+            m[rem] += 1;
+        }
+
+        return count;
+    }
+};
