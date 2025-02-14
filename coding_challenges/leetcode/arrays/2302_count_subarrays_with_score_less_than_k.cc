9fc299154bebb07cc68b37452231ad1f2180e80f
adetunjii <adetunjithomas1@outlook.com>
2025-02-14 23:57:00 +0100

    2302. Count subarrays with score less than k

--- coding_challenges/leetcode/arrays/2302_count_subarrays_with_score_less_than_k.cc
+++ coding_challenges/leetcode/arrays/2302_count_subarrays_with_score_less_than_k.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 class Solution {
     public:
-        long long countSubarrays(vector<int>& nums, long long k) {
+        long long countSubarraysBrute(vector<int>& nums, long long k) {
             int N = nums.size();
             long long result = 0;
     
@@ -16,6 +16,22 @@ class Solution {
                     if (score * (j - i + 1) < k) result += 1;
                     else break;
                 }
+            }
+            return result;
+        }
+
+        long long countSubarrays(vector<int>& nums, long long k) {
+            int N = nums.size(), l = 0;
+            long long result = 0, sum = 0;
+
+            for (int r = 0; r < N; r++) {
+                sum += nums[r];
+                while (sum * (r - l +1) >= k) {
+                    sum -= nums[l];
+                    l += 1;
+                }
+
+                result += r - l + 1;
             }
             return result;
         }
