4ef5c5fbd729513b376e71f0627346ddb8e71a4f
adetunjii <adetunjithomas1@outlook.com>
2026-01-06 14:36:04 +0000

    213. House Robber II

--- coding_challenges/leetcode/dp/213_house_robber_ii.cc
+++ coding_challenges/leetcode/dp/213_house_robber_ii.cc
@@ -1,22 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <span>
 #include <ranges>
 
 class Solution {
 public:
-    int rob(std::vector<int>& nums) {   
+    int rob(std::vector<int>& nums) {
         int N = nums.size();
 
         if (N == 1) return nums[0];
         if (N == 2) return std::max(nums[0], nums[1]);
 
+        std::span<int> v1(nums.data(), N-1);
+        std::span<int> v2(nums.data()+1, N-1);
+
+        return std::max(houseRobber(v1), houseRobber(v2));
+    }
+
+    int houseRobber(std::span<int> nums) {
+        int N = nums.size();
         std::vector<int> dp(N, 0);
+
         dp[0] = nums[0], dp[1] = std::max(nums[0], nums[1]);
 
         for (int i : std::views::iota(2, N)) {
             dp[i] = std::max(dp[i-1], dp[i-2] + nums[i]);
-        }
-        
-        return dp[N-1];
-    }
+       }
+
+       return dp[N-1];
+    }  
 };
