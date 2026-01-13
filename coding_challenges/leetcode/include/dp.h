a1ae309640518d6ff4b968df53a3d1d9c4a34663
adetunjii <adetunjithomas1@outlook.com>
2026-01-13 14:41:45 +0000

    chore: add tests for house robber

---
+++ coding_challenges/leetcode/include/dp.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+
+namespace dp {
+
+    // House Robber
+    int rob(std::vector<int>& nums);
+}
---
+++ coding_challenges/tests/leetcode/dynamic_programming/test_house_robber.cc
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <vector>
+#include <dp.h>
+
+TEST(TestDp, TestHouseRobber) {
+    std::vector<int> nums = {1,2,3,1};
+    // ASSERT_EQ(4, dp::rob(nums));
+}
