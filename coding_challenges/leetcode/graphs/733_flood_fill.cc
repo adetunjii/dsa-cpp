5d222da2bd9c77b54509765fc714b088d337adc7
adetunjii <adetunjithomas1@outlook.com>
2026-02-02 23:55:38 +0000

    733. Flood fill

---
+++ coding_challenges/leetcode/graphs/733_flood_fill.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+namespace graph {
+        void fill(std::vector<std::vector<int>>& image, int sr, int sc, int color, int newColor) {
+        if (
+            (sr < 0 || sr >= image.size() || 
+            sc < 0 || sc >= image[0].size()) || 
+            image[sr][sc] != color || image[sr][sc] == newColor
+        ) {
+            return;
+       }
+
+       image[sr][sc] = newColor;
+
+       fill(image,  sr-1, sc, color, newColor);
+       fill(image,  sr+1, sc, color, newColor);
+       fill(image,  sr, sc-1, color, newColor);
+       fill(image,  sr, sc+1, color, newColor);
+    }
+
+    std::vector<std::vector<int>> floodFill(std::vector<std::vector<int>>& image, int sr, int sc, int color) {
+        fill(image, sr, sc, image[sr][sc], color);
+        return image;
+    }
+}
