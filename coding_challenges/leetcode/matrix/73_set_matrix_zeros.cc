0201abc26c996e655edf5740b5bd7b936e717f73
adetunjii <adetunjithomas1@outlook.com>
2026-02-15 23:58:36 +0000

    73. Set matrix

--- coding_challenges/leetcode/matrix/73_set_matrix_zeros.cc
+++ coding_challenges/leetcode/matrix/73_set_matrix_zeros.cc
@@ -40,4 +40,48 @@ namespace matrix {
             sweepCol(matrix, c);
         }   
     }
+
+    void setZeroConstantSpace(std::vector<std::vector<int>>& matrix) {
+        int numRows = matrix.size(), numCols = matrix[0].size();
+
+        if (numRows == 0 || numCols == 0) return;
+
+        bool zeroFirstRow = false, zeroFirstCol = false;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (i == 0) {
+                    zeroFirstRow = zeroFirstRow || (matrix[i][j] == 0);
+                }
+
+                if (j == 0) {
+                    zeroFirstCol = zeroFirstCol || (matrix[i][j] == 0);
+                }
+
+
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < numRows; i++) {
+            for (int j = 1; j < numCols; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (zeroFirstRow) {
+            std::fill(matrix[0].begin(), matrix[0].end(), 0);
+        }
+
+        if (zeroFirstCol) {
+            for (int i = 0; i < numRows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
 }
