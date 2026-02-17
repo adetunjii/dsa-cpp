030de3077cf3ffb7d7aa4b020706017f5c6c79f2
adetunjii <adetunjithomas1@outlook.com>
2026-02-17 23:58:04 +0000

    20. Valid parentheses

--- coding_challenges/leetcode/stacks/20_valid_parentheses.cc
+++ coding_challenges/leetcode/stacks/20_valid_parentheses.cc
@@ -2,17 +2,15 @@
 #include <string>
 #include <stack>
 
-using namespace std;
-
 namespace stack{
-    bool isValid(string s) {
+    bool isValid(std::string s) {
         unordered_map<char, char> m = {
             {'{', '}'},
             {'(', ')'},
             {'[', ']'}
         };
 
-        stack<char> st;
+        std::stack<char> st;
         for (int i = 0; i < s.size(); i++) {
             if (!st.empty() && m[st.top()] == s[i]) {
                 st.pop();
