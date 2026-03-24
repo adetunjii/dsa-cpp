2930a860c7265a5f391b895b6b0d028c1e14d87d
adetunjii <adetunjithomas1@outlook.com>
2026-03-24 23:04:39 +0000

    279. Perfect squares

---
+++ coding_challenges/leetcode/dynamic_programming/279_perfect_squares.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <ranges>
+
+int numSquares(int N) {
+    std::vector<int> squares;
+
+    int i = 1;
+    while (i * i <= N) {
+        squares.push_back(i * i);
+        i += 1;
+    }
+    int n = squares.size();
+    
+    std::vector<int> dp(N+1, N+1);
+    dp[0] = 0;
+
+    for (int i : std::views::iota(1, n+1)) {
+        for (int c : std::views::iota(1, N + 1)) {
+            if (squares[i-1] <= c)
+                dp[c] = std::min(dp[c], dp[c-squares[i-1]] + 1);
+        }
+    }
+
+    return dp[N];
+}
