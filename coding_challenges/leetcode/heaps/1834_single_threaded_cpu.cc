9645c547f75a835353ee03d8acb8121859c6fb1e
adetunjii <adetunjithomas1@outlook.com>
2026-04-05 01:02:50 +0100

    1834. Single threaded cpu

--- coding_challenges/leetcode/heaps/1834_single_threaded_cpu.cc
+++ coding_challenges/leetcode/heaps/1834_single_threaded_cpu.cc
@@ -2,34 +2,34 @@
 #include <queue>
 #include <ranges>
 
+typedef std::pair<int, int> P;
+
 std::vector<int> getOrder(std::vector<std::vector<int>>& tasks) {
     int N = tasks.size();
 
-    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> pq;
-
-    for (int i : std::views::iota(0, N)) tasks[i].push_back(i);
-    
-    std::sort(tasks.begin(), tasks.end());
-    int running_time = 0;
-    
-    int i = 0;
+    std::priority_queue<P, std::vector<P>, std::greater<P>> pq;
 
     std::vector<int> res;
+    res.reserve(N);
+    int running_time = 0, i = 0;
 
-    while (i < N || pq.size()) {
+    for (int i : std::views::iota(0, N)) tasks[i].push_back(i);
+
+    std::sort(tasks.begin(), tasks.end());
+
+    while (i < N || pq.size() > 0) {
         if (pq.empty()) {
             running_time = std::max(running_time, tasks[i][0]);
         }
 
-        while (i < N && running_time >= tasks[i][0]) {
+        while (i < N || running_time >= tasks[i][0]) {
             pq.emplace(tasks[i][1], tasks[i][2]);
             i += 1;
         }
 
-        auto [proc, idx] = pq.top();
+        running_time += (pq.top().first);
+        res.push_back(pq.top().second);
         pq.pop();
-        running_time += proc;
-        res.push_back(idx);
     }
 
     return res;
