5bca40ced965984194f5ca402d4d3edc0d3149e0
adetunjii <adetunjithomas1@outlook.com>
2026-04-05 01:03:23 +0100

    1834. Single threaded cpu

--- coding_challenges/leetcode/heaps/1834_single_threaded_cpu.cc
+++ coding_challenges/leetcode/heaps/1834_single_threaded_cpu.cc
@@ -11,7 +11,7 @@ std::vector<int> getOrder(std::vector<std::vector<int>>& tasks) {
 
     std::vector<int> res;
     res.reserve(N);
-    int running_time = 0, i = 0;
+    long running_time = 0, i = 0;
 
     for (int i : std::views::iota(0, N)) tasks[i].push_back(i);
 
@@ -19,7 +19,7 @@ std::vector<int> getOrder(std::vector<std::vector<int>>& tasks) {
 
     while (i < N || pq.size() > 0) {
         if (pq.empty()) {
-            running_time = std::max(running_time, tasks[i][0]);
+            running_time = std::max(running_time, (long)tasks[i][0]);
         }
 
         while (i < N || running_time >= tasks[i][0]) {
