8385003af2b033ea71892345a455645afc3725e8
adetunjii <adetunjithomas1@outlook.com>
2026-04-11 23:57:10 +0100

    remove unsued code from main file

--- main.cc
+++ main.cc
@@ -7,19 +7,4 @@
 
 using namespace std;
 
-int main() {
-    int f[3] = {3, 2, 1};
-    int g[5] = {1, 2, 3, 4, 5};
-
-    // find the convolution after the 3rd day
-    int days = 3;
-    for (int x = 0; x < days; x++) {
-        int sum = 0;
-
-        for (int i = 0; i < 5; i++) {
-            if (i < x) {
-            }
-            sum += f[i] + g[-i + x];
-        }
-    }
-}
+int main() {}
