419fd74f5f27fe94498bac3ecc2a0b94380484db
adetunjii <adetunjithomas1@outlook.com>
2026-04-21 23:55:38 +0100

    653. Two Sum IV - Input is a BST

--- coding_challenges/leetcode/trees/653_two_sum_iv_input_is_a_bst.cc
+++ coding_challenges/leetcode/trees/653_two_sum_iv_input_is_a_bst.cc
@@ -1,15 +1,6 @@
 #include <vector>
 #include <unordered_map>
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
+#include "treenode.h"
 
 void inorder(TreeNode* root, std::vector<int>& res) {
     if (root == nullptr) return;
