4220c3ffd9e84c2df3c0db82faa3ed55f43c2b29
adetunjii <adetunjithomas1@outlook.com>
2026-04-22 12:15:13 +0100

    501. Find Mode in Binary search Tree

--- coding_challenges/leetcode/trees/501_find_mode_in_binary_search_tree.cc
+++ coding_challenges/leetcode/trees/501_find_mode_in_binary_search_tree.cc
@@ -2,73 +2,104 @@
 #include <unordered_map>
 #include "treenode.h"
 
-void preorder(TreeNode* root, std::vector<int>& vec) {
-    if (root == nullptr) return;
-
-    vec.push_back(root->val);
-    preorder(root->left, vec);
-    preorder(root->right, vec);
-}
-
-vector<int> findMode(TreeNode* root) {
-    std::unordered_map<int, int> m;
-
-    std::vector<int> vec;
-
-    preorder(root, vec);
-
-    for (int num : vec) {
-        if (m.count(num) > 0) {
-            m[num] += 1;
-        } else {
-            m[num] = 1;
-        }
-    }
-
-    int currentMaxCount = INT_MIN;
+class FindMode {
+public:
+    int maxCount = 0;
+    int count = 0;
+    unordered_map<int, int> m;
     vector<int> result;
-    for (auto [key, val] : m) {
-        if (val > currentMaxCount) {
-            currentMaxCount = val;
+    TreeNode* node = nullptr;
+
+    void preorder(TreeNode* root, std::vector<int>& vec) {
+        if (root == nullptr) return;
+
+        vec.push_back(root->val);
+        preorder(root->left, vec);
+        preorder(root->right, vec);
+    }
+
+    vector<int> findMode(TreeNode* root) {
+        std::unordered_map<int, int> m;
+
+        std::vector<int> vec;
+
+        preorder(root, vec);
+
+        for (int num : vec) {
+            if (m.count(num) > 0) {
+                m[num] += 1;
+            } else {
+                m[num] = 1;
+            }
         }
-    }
-    
 
-    for (auto [key, val] : m) {
-        if (val == currentMaxCount) {
-            result.push_back(key);
+        int currentMaxCount = INT_MIN;
+        vector<int> result;
+        for (auto [key, val] : m) {
+            if (val > currentMaxCount) {
+                currentMaxCount = val;
+            }
         }
+        
+
+        for (auto [key, val] : m) {
+            if (val == currentMaxCount) {
+                result.push_back(key);
+            }
+        }
+
+        return result;
     }
 
-    return result;
-}
+    void inorderWithMap(TreeNode* root) {
+        if (root == nullptr) return;
 
-int maxCount = 0;
-unordered_map<int, int> m;
-vector<int> result;
+        inorderWithMap(root->left);
 
-void inorder(TreeNode* root) {
-    if (root == nullptr) return;
+        if (m.count(root->val) > 0) {
+            m[root->val] += 1;
+        } else {
+            m[root->val] = 1;
+        }
 
-    inorder(root->left);
+        if (m[root->val] > maxCount) {
+            result = {root->val};
+            maxCount = m[root->val];
+        } else if (m[root->val] == maxCount) {
+            result.push_back(root->val);
+        }
 
-    if (m.count(root->val) > 0) {
-        m[root->val] += 1;
-    } else {
-        m[root->val] = 1;
+        inorderWithMap(root->right);
     }
 
-    if (m[root->val] > maxCount) {
-        result = {root->val};
-        maxCount = m[root->val];
-    } else if (m[root->val] == maxCount) {
-        result.push_back(root->val);
+    void inorderOptimal(TreeNode* root) {
+        if (root == nullptr) {
+            return;
+        }
+
+        inorderOptimal(root->left);
+
+        if (node == nullptr || node->val != root->val) {
+            count = 1;
+        } else {
+            count += 1;
+        }
+
+        node == root;
+
+        if (count > maxCount) {
+            result = {root->val};
+            maxCount = count;
+        } else if (count == maxCount) {
+            result.push_back(root->val);
+        }
+
+        inorderOptimal(root->right);
     }
 
-    inorder(root->right);
-}
 
-vector<int> findModeInOrder(TreeNode* root) {
-    inorder(root);
-    return result;
-}
+    vector<int> findModeInOrder(TreeNode* root) {
+        inorderOptimal(root);
+        return result;
+    }
+};
