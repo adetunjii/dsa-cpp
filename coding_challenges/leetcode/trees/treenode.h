2926a20dc1dcfdde094bda8ef3b302f04fb9c925
adetunjii <adetunjithomas1@outlook.com>
2026-04-24 22:15:08 +0100

    N-ary tree postorder traversal

--- coding_challenges/leetcode/trees/treenode.h
+++ coding_challenges/leetcode/trees/treenode.h
@@ -1,8 +1,8 @@
 #ifndef TREENODE_H
 #define TREENODE_H
 
-#include <vector>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
@@ -18,15 +18,16 @@ struct TreeNode {
 };
 
 TreeNode* TreeNode::buildTree(vector<int>& arr) {
-    if (arr.empty()) return nullptr;
+    if (arr.empty())
+        return nullptr;
 
     TreeNode* root = new TreeNode(arr[0]);
     queue<TreeNode*> q;
     q.push(root);
 
     int i = 1;
-    
-    while(!q.empty() && i < arr.size()) {
+
+    while (!q.empty() && i < arr.size()) {
         auto node = q.front();
         q.pop();
 
@@ -48,4 +49,21 @@ TreeNode* TreeNode::buildTree(vector<int>& arr) {
 
 void TreeNode::deleteTree(TreeNode* node) {}
 
-#endif //TREENODE_H
+class Node {
+public:
+    int val;
+    std::vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, std::vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+#endif // TREENODE_H
