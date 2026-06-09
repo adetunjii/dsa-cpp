5c64582fd93397e01f36c68e127825a149090d0c
adetunjii <adetunjithomas1@outlook.com>
2026-06-09 23:51:51 +0100

    563. Binary Tree Tilt

--- coding_challenges/leetcode/trees/563_binary_tree_tilt.cc
+++ coding_challenges/leetcode/trees/563_binary_tree_tilt.cc
@@ -1,20 +1,18 @@
-#include <iostream>
 #include "treenode.h"
+#include <stdlib.h>
 
-class Solution {
-private:
-	int calc(TreeNode* node, int& sum) {
-		if (node == nullptr) return 0;
+int calc(TreeNode* node, int& sum) {
+    if (node == nullptr)
+        return 0;
 
-		int left = calc(node->left, sum);
-		int right = calc(node->right, sum);
-		sum += abs(left-right);
-		return left + right + node->val;
-	}
-public:
-		int findTilt(TreeNode* root) {
-			int sum = 0;
-			calc(root, sum);
-			return sum;
-		}
-};
+    int left = calc(node->left, sum);
+    int right = calc(node->right, sum);
+    sum += abs(left - right);
+    return left + right + node->val;
+}
+
+int findTilt(TreeNode* root) {
+    int sum = 0;
+    calc(root, sum);
+    return sum;
+}
