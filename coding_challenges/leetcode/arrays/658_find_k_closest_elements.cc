c829053c21f7a6856472a70f34139a22c35869a3
adetunjii <adetunjithomas1@outlook.com>
2026-06-22 01:51:58 +0100

    658. Find k Closest Elements

--- coding_challenges/leetcode/arrays/658_find_k_closest_elements.cc
+++ coding_challenges/leetcode/arrays/658_find_k_closest_elements.cc
@@ -1,5 +1,28 @@
 #include <vector>
 
+std::vector<int> findClosestElementsTwoPointer(std::vector<int>& A, int k, int x) {
+    int N = A.size();
+    int right = lower_bound(A.begin(), A.end(), x) - A.begin();
+    int left = right - 1;
+
+    for (int i = 0; i < k; i++) {
+        if (left < 0) {
+            right += 1;
+        } else if (right >= N) {
+            left -= 1;
+        } else {
+            if (x - A[left] <= A[right] - x) {
+                left -= 1;
+            } else {
+                right += 1;
+            }
+        }
+    }
+
+    // we use left + 1 because we need to trim the excess loop step at the end.
+    return std::vector<int>(A.begin() + left + 1, A.begin() + right);
+}
+
 std::vector<int> findClosestElements(std::vector<int>& A, int k, int x) {
     int left = 0;
     int right = A.size() - k;
