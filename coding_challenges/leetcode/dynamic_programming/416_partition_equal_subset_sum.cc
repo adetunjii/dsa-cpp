89a4644cfb59bdb9d5e65a82c19e61c8cf18ebf8
adetunjii <adetunjithomas1@outlook.com>
2026-09-08 23:51:05 +0100

    416. Partition equal subset sum

--- coding_challenges/leetcode/dynamic_programming/416_partition_equal_subset_sum.cc
+++ coding_challenges/leetcode/dynamic_programming/416_partition_equal_subset_sum.cc
@@ -1,43 +1,44 @@
 #include <iostream>
-#include <vector>
+#include <numeric>
 #include <ranges>
+#include <vector>
 
 namespace dp {
-    bool canPartition(std::vector<int>& nums) {
-        int N = nums.size();
-        if (N < 2) return false;
-
-        int total = 0;
-        for (auto& num : nums) {
-            total += num;
-        }
-
-        if (total % 2 != 0) return false;
-
-        int target = total/2;
-
-        // can be used to replace code below        
-        // std::vector<int>dp(target+1, 0);
-        // for (int i : std::views::iota(0, N)) {
-        //     for (int cap = target; cap >= 0; cap--) {
-        //         if (cap >= nums[i]) {
-        //             dp[cap] = std::max(dp[cap], dp[cap-nums[i]] + nums[i]);
-        //         }
-        //     }
-        // }
-        // return dp[target] == target;
-
-        std::vector<bool> dp(target+1, false);
-        dp[0] = true;
-
-        for (int i : std::views::iota(0, N)) {
-            for (int cap : std::views::iota(1, target+1) | std::views::reverse) {
-                if (cap >= nums[i]) {
-                    dp[cap] = dp[cap] || nums[i] + dp[cap - nums[i]];
-                }
-            }
-        }
-
-        return dp[target];
+bool canPartition(std::vector<int>& nums) {
+    int total = std::accumulate(nums.begin(), nums.end(), 0);
+    if (total & 1 > 0) {
+        return false;
     }
+
+    int target = total >> 1;
+
+    std::vector<bool> dp(target + 1, false);
+    dp[0] = true;
+
+    for (int num : nums) {
+        for (int cap = target + 1; cap >= num; cap--) {
+            dp[cap] = dp[cap] || dp[cap - num];
+        }
+    }
+    return dp[target];
 }
+
+bool canPartitionBitset(std::vector<int>& nums) {
+    int total = std::accumulate(nums.begin(), nums.end(), 0);
+    if (total & 1 > 0) {
+        return false;
+    }
+
+    int target = total >> 1;
+
+    std::vector<bool> dp(target + 1, false);
+
+    std::bitset<10001> bit(1);
+
+    for (int num : nums) {
+        bit |= bit << num;
+    }
+
+    return bit[target];
+}
+} // namespace dp
