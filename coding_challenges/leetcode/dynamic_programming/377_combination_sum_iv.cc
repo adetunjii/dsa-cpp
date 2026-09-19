0446bcfa1afa36f316290f1cfddd62a15ba49c34
adetunjii <adetunjithomas1@outlook.com>
2026-09-19 15:21:36 +0100

    377. Combination Sum IV

---
+++ coding_challenges/leetcode/dynamic_programming/377_combination_sum_iv.cc
@@ -0,0 +1,17 @@
+#include <vector>
+namespace dp {
+    int combinationSum4(std::vector<int>& nums, int target) {
+        std::vector<long long> dp(target+1, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (i >= num && dp[i] <= INT_MAX) {
+                    dp[i] += dp[i-num];
+                }
+            }
+        }
+
+        return dp[target];
+    }
+}
