add4e1727e697ba8cb89efe3d0bd0ae6de954e4a
adetunjii <adetunjithomas1@outlook.com>
2026-09-21 23:58:25 +0100

    152. Maximum Subarray Product

--- coding_challenges/leetcode/arrays/152_maximum_subarray_product.cc
+++ coding_challenges/leetcode/arrays/152_maximum_subarray_product.cc
@@ -2,30 +2,28 @@
 #include <vector>
 
 using namespace std;
+namespace dp{
+int maxProduct(vector<int>& nums) {
+    int mp = INT_MIN, N = nums.size();
+    int cur = 1;
 
-class Solution {
-public:
-    int maxProduct(vector<int>& nums) {
-        int mp = INT_MIN, N = nums.size();
-        int cur = 1;
+    for(int num : nums) {
+        cur *= num;
+        mp = max(mp, cur);
 
-        for(int num : nums) {
-            cur *= num;
-            mp = max(mp, cur);
-
-            if (cur == 0) { // start a new subarray
-                cur = 1;
-            }
+        if (cur == 0) { // start a new subarray
+            cur = 1;
         }
-
-        cur = 1;
-        for (int i = N - 1; i >= 0; i--) {
-            cur *= nums[i];
-            mp = max(mp, cur);
-
-            if (cur == 0)
-                cur = 1;
-        }
-        return mp;
     }
+
+    cur = 1;
+    for (int i = N - 1; i >= 0; i--) {
+        cur *= nums[i];
+        mp = max(mp, cur);
+
+        if (cur == 0)
+            cur = 1;
+    }
+    return mp;
+}
 };
